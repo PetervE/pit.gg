72f898c552721e1de2ae7932b8641be77c2a2666
PetervE <peter@fearless-apps.com>
2021-03-06 20:23:31 +0100

    position

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,33 @@ export default function Home() {
 
   const [workoutData, setWorkoutData] = useState([]);
 
+  const toggleColorMode = () => {
+    console.log("current", localStorage.theme);
+    if (localStorage.theme === "light") {
+      localStorage.theme = "dark";
+      document.documentElement.classList.add("dark");
+      setDarkModeActive(true);
+    } else {
+      localStorage.theme = "light";
+      document.documentElement.classList.remove("dark");
+      setDarkModeActive(false);
+    }
+  };
+
+  const checkLocalStorage = () => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+      setDarkModeActive(true);
+    } else {
+      document.documentElement.classList.remove("dark");
+      setDarkModeActive(false);
+    }
+  };
+
   useEffect(() => {
     checkLocalStorage();
     getLiftLogs();
@@ -22,6 +49,7 @@ export default function Home() {
     if (workouts) drawGraph();
   }, [workouts]);
 
+  // get workouts, excercises and set active exercise
   const getLiftLogs = async () => {
     try {
       const logs = await Storage.get("logs/fithero-backup-2021-03-05.json", {
@@ -105,33 +133,6 @@ export default function Home() {
     ]);
   };
 
-  const checkLocalStorage = () => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      document.documentElement.classList.add("dark");
-      setDarkModeActive(true);
-    } else {
-      document.documentElement.classList.remove("dark");
-      setDarkModeActive(false);
-    }
-  };
-
-  const toggleColorMode = () => {
-    console.log("current", localStorage.theme);
-    if (localStorage.theme === "light") {
-      localStorage.theme = "dark";
-      document.documentElement.classList.add("dark");
-      setDarkModeActive(true);
-    } else {
-      localStorage.theme = "light";
-      document.documentElement.classList.remove("dark");
-      setDarkModeActive(false);
-    }
-  };
-
   const changeExercise = (e) => {
     setActiveExercise(e);
     drawGraph();
@@ -139,30 +140,28 @@ export default function Home() {
 
   if (!exercises || !activeExercise) return null;
 
-  let exercisesList = Object.keys(exercises);
-
   return (
-    <div className="bg-white dark:bg-gray-800">
+    <div className="flex flex-wrap flex-col bg-white dark:bg-gray-800">
       <Head>
         <title>Peter van Egeraat</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav className="bg-blue-400 dark:bg-red-400">
+      <nav className="flex flex-grow-0 bg-blue-400 dark:bg-red-400">
         <button onClick={toggleColorMode}>Toggle</button>
       </nav>
-      <header className="flex flex-wrap justify-center">
+      <header className="flex flex-grow-0 justify-center">
         <div className="w-96 m-8">
           <Select
             value={activeExercise}
             onChange={changeExercise}
-            options={exercisesList.reduce((memo, e) => {
+            options={Object.keys(exercises).reduce((memo, e) => {
               memo.push({ value: e, label: e.replaceAll("-", " ") });
               return memo;
             }, [])}
           />
         </div>
       </header>
-      <div className="h-screen flex flex-col flex-wrap justify-center content-center">
+      <div className="h-screen flex flex-col flex-wrap justify-start content-center">
         <div className="flex flex-wrap w-full h-2/4">
           <ResponsiveLine
             data={workoutData}
